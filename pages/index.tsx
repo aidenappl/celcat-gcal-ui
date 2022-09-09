e447ac4fce3b846d6947cbce2ba9c6dc941f3fa7
Aiden Appleby <aiden.appleby3@gmail.com>
2022-09-09 20:31:46 +0100

    added mobile responsivitivity

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ const Home: NextPage = () => {
   const [email, setEmail] = useState("");
   const [link, setLink] = useState("");
   const [loading, setLoading] = useState(false);
+  const [buttonValue, setButtonValue] = useState("Copy Link");
 
   const CheckLinkValidity = async () => {
     const response = schema.validate({ email });
@@ -59,11 +60,20 @@ const Home: NextPage = () => {
 
       {link ? (
         <div className="flex items-center justify-center w-full h-screen">
-          <div className="w-[500px] h-fit bg-white rounded-md p-10 z-10">
+          <div className="w-full sm:w-[500px] h-fit bg-white sm:rounded-md p-7 sm:p-10 z-10 flex flex-col">
             <h1 className="text-lg font-semibold">Here is your link</h1>
-            <a className={"text-blue-500 cursor-pointer"} href={link}>
+            <a className={"text-blue-500 cursor-pointer break-all"} href={link}>
               {link}
             </a>
+            <button
+              className="w-fit h-fit px-5 py-1 bg-blue-600 text-white rounded-md mt-3"
+              onClick={() => {
+                setButtonValue("Copied!");
+                navigator.clipboard.writeText(link);
+              }}
+            >
+              {buttonValue}
+            </button>
             <p className="pt-10">
               Copy and paste this into your respective calendar or download the
               ICS file by going to the url.
@@ -83,16 +93,21 @@ const Home: NextPage = () => {
         </div>
       ) : null}
 
-      <main className="flex items-center justify-center w-full h-screen">
+      <main className="flex items-center justify-center w-full h-screen flex-col sm:flex-row px-5">
         <input
-          className="w-[400px] h-[50px] border-2 pl-4 rounded-md"
+          className="w-full sm:w-[400px] h-[50px] border-2 pl-4 rounded-md"
           type={"text"}
           placeholder={"Enter your Northeastern Email"}
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              CheckLinkValidity();
+            }
+          }}
         />
         <button
-          className="w-[175px] h-[50px] bg-blue-600 text-white ml-5 rounded-md flex items-center justify-center"
+          className="w-full mt-5 sm:mt-0 sm:w-[175px] h-[50px] bg-blue-600 text-white sm:ml-5 rounded-md flex items-center justify-center"
           onClick={() => {
             CheckLinkValidity();
           }}
